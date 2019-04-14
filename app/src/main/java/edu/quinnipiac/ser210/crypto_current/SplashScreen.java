52b6d1b66be235d84720d8436eccbc878543b8ac
Kevin <kevin.sangurima@quinnipiac.edu>
2019-04-14 11:15:23 -0400

    Original

---
+++ app/src/main/java/edu/quinnipiac/ser210/crypto_current/SplashScreen.java
@@ -0,0 +1,13 @@
+package edu.quinnipiac.ser210.crypto_current;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+    }
+}
