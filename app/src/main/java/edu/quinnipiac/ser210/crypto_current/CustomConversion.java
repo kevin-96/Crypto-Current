638a051d86782b6e5caf8c2bfa55e2c6d0576532
Brian Carballo <brian.carballo@quinnipiac.edu>
2019-04-16 19:18:59 -0400

    Working Tab Layout (WOO!)
    ********
    * TODO *
    ********
    
    - Delete 3rd tab
    - Clear unnecessary code
    - Connect to navigation from splash

---
+++ app/src/main/java/edu/quinnipiac/ser210/crypto_current/CustomConversion.java
@@ -0,0 +1,45 @@
+package edu.quinnipiac.ser210.crypto_current;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CustomConversion extends Fragment {
+
+
+    public CustomConversion() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View layout = inflater.inflate(R.layout.fragment_custom_conversion, container, false);
+
+        String[] coinSpinnerValues = new String[] {"BTC", "EUR"};
+        Spinner coinSpinner = (Spinner) layout.findViewById(R.id.coinChooseSpinner);
+        ArrayAdapter<String> coinAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item, coinSpinnerValues);
+        coinAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        coinSpinner.setAdapter(coinAdapter);
+
+        String[] currencySpinnerValues = new String[] {"AUD", "USD", "CAD"};
+        Spinner currencySpinner = (Spinner) layout.findViewById(R.id.currencyChooseSpinner);
+        ArrayAdapter<String> currencyAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item, currencySpinnerValues);
+        currencyAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        currencySpinner.setAdapter(currencyAdapter);
+
+        return layout;
+    }
+
+}
--- app/src/main/java/edu/quinnipiac/ser210/crypto_current/SplashScreen.java
+++ app/src/main/java/edu/quinnipiac/ser210/crypto_current/SplashScreen.java
@@ -21,6 +21,6 @@ public class SplashScreen extends AppCompatActivity implements SplashScreenFragm
 
     @Override
     public void onFragmentInteraction(Uri uri) {
-
+        
     }
 }
