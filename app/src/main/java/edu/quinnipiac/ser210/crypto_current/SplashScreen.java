c9f0ccd27cd5234389bae824c83330723f6f7869
Kevin <kevin.sangurima@quinnipiac.edu>
2019-04-16 17:47:23 -0400

    Original

--- app/src/main/java/edu/quinnipiac/ser210/crypto_current/SplashScreen.java
+++ app/src/main/java/edu/quinnipiac/ser210/crypto_current/SplashScreen.java
@@ -1,14 +1,26 @@
 package edu.quinnipiac.ser210.crypto_current;
 
+
+import android.net.Uri;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SplashScreen extends AppCompatActivity {
+public class SplashScreen extends AppCompatActivity implements SplashScreenFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
+
+
+        SplashScreenFragment mFragment = new SplashScreenFragment();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.add(R.id.splashFragmentContainer, mFragment).commit();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
 
     }
 }
